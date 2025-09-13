5c91ba27d7af27b04eec0c32288d58ab51df3d6b
sresri2 <ssr075815@gmail.com>
2025-09-13 02:26:57 -0700

    Adjustment to NLP processor
    Added filter for misidentification of garbled characters as informational words.
    
    Updated spacy NLP to more powerful model

--- description_processor.py
+++ description_processor.py
@@ -1,21 +1,22 @@
 import spacy
 import re
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_trf")
 
 def clean_entities(entities):
     keywords = []
+    seen = set()
     for ent in entities:
         for part in re.split(r"[&/,]", ent):
             part = part.strip()
-            if part and len(part) > 2:
-                keywords.append(part)
+            if part and len(part) > 2 and not re.search(r"[@:/.]", part):
+                key = part.lower()
+                if key not in seen:
+                    seen.add(key)
+                    keywords.append(part)
     return keywords
 
 def extract_search_terms(description):
     doc = nlp(description)
-
-    entities = [ent.text for ent in doc.ents if ent.label_ in ["ORG", "PRODUCT", "WORK_OF_ART"]]
-
-    res = clean_entities(entities)
-    return res
+    entities = [ent.text for ent in doc.ents if ent.label_ in ["ORG", "PRODUCT"]]
+    return clean_entities(entities)
